31045346041c6692ddee0adaf7d73d945700e86a
chonuba <chonuba@gmail.com>
2024-08-03 23:28:25 +0100

    vpc config modified

--- terraform-eks/vpc.tf
+++ terraform-eks/vpc.tf
@@ -20,11 +20,11 @@ module "vpc" {
 
   name                 = "sock-shop-vpc"
   cidr                 = var.vpc_cidr
-  azs                  = ["eu-north-1a"]
-  private_subnets      = ["10.0.1.0/24"]
-  public_subnets       = ["10.0.2.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  azs                  = ["eu-north-1a", "eu-north-1b"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
+  #enable_nat_gateway   = true
+  #single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
 
