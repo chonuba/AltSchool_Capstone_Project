8e2a84bec87887a920fd9633d8a358f036bf1137
chonuba <chonuba@gmail.com>
2024-08-04 22:45:17 +0100

    terraform-eks deleted, eks-terraform created

---
+++ eks-terraform/eks-cluster.tf
@@ -0,0 +1,61 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.eks_cluster_name
+  cluster_version = var.kubernetes_version
+  subnet_ids      = module.vpc.private_subnets
+
+  cluster_endpoint_public_access  = true
+
+  vpc_id = module.vpc.vpc_id
+
+  # EKS Managed Node Group(s)
+#   eks_managed_node_group_defaults = {
+#     instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+#   }
+
+  eks_managed_node_groups = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
+
+#   access_entries = {
+#     # One access entry with a policy associated
+#     example = {
+#       kubernetes_groups = []
+#       principal_arn     = "arn:aws:iam::123456789012:role/something"
+
+#       policy_associations = {
+#         example = {
+#           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+#           access_scope = {
+#             namespaces = ["default"]
+#             type       = "namespace"
+#           }
+#         }
+#       }
+#     }
+#   }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+# resource "null_resource" "kubectl" {
+#     provisioner "local-exec" {
+#         command = "aws eks --region ${var.aws_region} update-kubeconfig --name ${var.eks_cluster_name}"
+#     }
+# }
--- eks-terraform/outputs.tf
+++ eks-terraform/outputs.tf
@@ -8,19 +8,19 @@ output "cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane."
-  value       = module.eks.cluster_security_group_id
-}
+# output "cluster_security_group_id" {
+#   description = "Security group ids attached to the cluster control plane."
+#   value       = module.eks.cluster_security_group_id
+# }
 
 output "region" {
   description = "AWS region"
   value       = var.aws_region
 }
 
-output "oidc_provider_arn" {
-  value = module.eks.oidc_provider_arn
-}
+# output "oidc_provider_arn" {
+#   value = module.eks.oidc_provider_arn
+# }
 
 #output "zz_update_kubeconfig_command" {
   # value = "aws eks update-kubeconfig --name " + module.eks.cluster_id
