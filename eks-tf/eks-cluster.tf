f0bd19c9b138ee1e4817ac1b2639df7a06c0bc4e
chonuba <chonuba@gmail.com>
2024-08-05 14:47:57 +0100

    more update to outputs.tf and eks-clusters.tf

--- eks-tf/eks-cluster.tf
+++ eks-tf/eks-cluster.tf
@@ -5,26 +5,37 @@ module "eks" {
   cluster_name    = "sock-shop-eks"
   cluster_version = "1.30"
 
-#   cluster_endpoint_public_access  = true
+  cluster_endpoint_public_access  = true
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   #control_plane_subnet_ids = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
 
   # EKS Managed Node Group(s)
-#   eks_managed_node_group_defaults = {
-#     instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
-#   }
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.micro"]
+
+  }
 
   eks_managed_node_groups = {
-    sock-shop-nodes = {
+    sock-shop-nodes-1 = {
+      name = "sock-shop-group-1"
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["t3.medium"]
+      # ami_type       = "AL2023_x86_64_STANDARD"
+      # instance_types = ["t3.micro"]
 
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      min_size     = 2
+      max_size     = 4
+      desired_size = 4
+    }
+
+    sock-shop-nodes-2 = {
+      name = "sock-shop-group-2"
+
+      min_size     = 2
+      max_size     = 4
+      desired_size = 4
     }
   }
 
@@ -37,3 +48,9 @@ module "eks" {
     Terraform   = "true"
   }
 }
+
+# resource "null_resource" "kubectl" {
+#     provisioner "local-exec" {
+#         command = "aws eks --region eu-north-1 update-kubeconfig --name sock-shop-eks"
+#     }
+# }
